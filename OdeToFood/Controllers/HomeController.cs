aa701446ab2979291c038bb8c6a5cedf9ea14226
Opilane TTHK <opilane@tthk.ee>
2021-12-07 09:50:40 +0200

    odetofood

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore.Unobtrusive.Ajax;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using OdeToFood.Data;
 using OdeToFood.Models;
@@ -32,6 +34,11 @@ namespace OdeToFood.Controllers
 					Country = r.Country,
 					CountOfReviews = r.Reviews.Count()
 				});
+
+            if (Request.IsAjaxRequest())
+            {
+				return PartialView("_Restaurants", model);
+            }
 
 			//var model = _context.Restaurants.OrderByDescending(
 			//	r => r.Reviews.Average(review => review.Rating)
--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,16 +1,29 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Models
 {
-    public class RestaurantReview
-    {
-        public int Id { get; set; }
-        public int Rating { get; set; }
-        public string Body { get; set; }
-        public int RestaurantId { get; set; }
-    }
+	public class RestaurantReview : IValidatableObject
+	{
+		public int Id { get; set; }
+		[Range(1, 10)]
+		public int Rating { get; set; }
+		[Required]
+		[StringLength(1024)]
+		public string Body { get; set; }
+		[Display(Name = "User Name")]
+		[DisplayFormat(NullDisplayText = "anonymous")]
+		public string ReviewerName { get; set; }
+		public int RestaurantId { get; set; }
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (Rating < 2 && Body.Length < 50)
+			{
+				yield return new ValidationResult("Low reviews need longer body");
+			}
+		}
+	}
 }
