3ac5ecc9c14e9a569ea241c4ce9535bd266a3d74
kivikangur-tthk <kristjan.kivikangur@tthk.ee>
2021-12-14 11:25:21 +0200

    Add paging

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace OdeToFood.Controllers
 {
@@ -35,13 +36,13 @@ namespace OdeToFood.Controllers
 			return Json(model);
 		}
 
-		public IActionResult Index(string searchTerm = null)
+		public IActionResult Index(string searchTerm = null, int page = 1)
 		{
 			var model = _context.Restaurants
 				.OrderByDescending(
 					r => r.Reviews.Average(review => review.Rating)
 					)
-				.Where(r=>searchTerm==null || r.Name.Contains(searchTerm))
+				.Where(r => searchTerm == null || r.Name.Contains(searchTerm))
 				.Select(r => new RestaurantListViewModel
 				{
 					Id = r.Id,
@@ -49,8 +50,9 @@ namespace OdeToFood.Controllers
 					City = r.City,
 					Country = r.Country,
 					CountOfReviews = r.Reviews.Count
-				});
+				}).ToPagedList(page,10);
 
+			
 			if (Request.IsAjaxRequest())
 			{
 				return PartialView("_Restaurants", model);
