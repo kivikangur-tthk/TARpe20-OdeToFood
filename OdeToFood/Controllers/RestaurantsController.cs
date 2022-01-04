b160452739970db156883a2dc6dec0b2708e2c19
kivikangur-tthk <kristjan.kivikangur@tthk.ee>
2022-01-04 14:11:38 +0200

    Add Authorization to restaurants controller

--- OdeToFood/Controllers/RestaurantsController.cs
+++ OdeToFood/Controllers/RestaurantsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,144 +11,146 @@ using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
-    public class RestaurantsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	[Authorize]
+	public class RestaurantsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public RestaurantsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public RestaurantsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Restaurants
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Restaurants.ToListAsync());
-        }
+		// GET: Restaurants
+		[AllowAnonymous]
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Restaurants.ToListAsync());
+		}
 
-        // GET: Restaurants/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Restaurants/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var restaurant = await _context.Restaurants
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (restaurant == null)
-            {
-                return NotFound();
-            }
+			var restaurant = await _context.Restaurants
+							.FirstOrDefaultAsync(m => m.Id == id);
+			if (restaurant == null)
+			{
+				return NotFound();
+			}
 
-            return View(restaurant);
-        }
+			return View(restaurant);
+		}
 
-        // GET: Restaurants/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Restaurants/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Restaurants/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,City,Country")] Restaurant restaurant)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(restaurant);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(restaurant);
-        }
+		// POST: Restaurants/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,City,Country")] Restaurant restaurant)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(restaurant);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(restaurant);
+		}
 
-        // GET: Restaurants/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Restaurants/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var restaurant = await _context.Restaurants.FindAsync(id);
-            if (restaurant == null)
-            {
-                return NotFound();
-            }
-            return View(restaurant);
-        }
+			var restaurant = await _context.Restaurants.FindAsync(id);
+			if (restaurant == null)
+			{
+				return NotFound();
+			}
+			return View(restaurant);
+		}
 
-        // POST: Restaurants/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,City,Country")] Restaurant restaurant)
-        {
-            if (id != restaurant.Id)
-            {
-                return NotFound();
-            }
+		// POST: Restaurants/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,City,Country")] Restaurant restaurant)
+		{
+			if (id != restaurant.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(restaurant);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!RestaurantExists(restaurant.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(restaurant);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(restaurant);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!RestaurantExists(restaurant.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(restaurant);
+		}
 
-        // GET: Restaurants/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Restaurants/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var restaurant = await _context.Restaurants
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (restaurant == null)
-            {
-                return NotFound();
-            }
+			var restaurant = await _context.Restaurants
+							.FirstOrDefaultAsync(m => m.Id == id);
+			if (restaurant == null)
+			{
+				return NotFound();
+			}
 
-            return View(restaurant);
-        }
+			return View(restaurant);
+		}
 
-        // POST: Restaurants/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var restaurant = await _context.Restaurants.FindAsync(id);
-            _context.Restaurants.Remove(restaurant);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Restaurants/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var restaurant = await _context.Restaurants.FindAsync(id);
+			_context.Restaurants.Remove(restaurant);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool RestaurantExists(int id)
-        {
-            return _context.Restaurants.Any(e => e.Id == id);
-        }
-    }
+		private bool RestaurantExists(int id)
+		{
+			return _context.Restaurants.Any(e => e.Id == id);
+		}
+	}
 }
